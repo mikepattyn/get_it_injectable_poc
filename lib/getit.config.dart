833d83ae4cbbc9c393c96fb8c7499ce4c9468a1b
Mike Pattyn <mike.pattyn1@gmail.com>
2023-08-14 21:48:17 +0200

    checkin temp fix

--- lib/getit.config.dart
+++ lib/getit.config.dart
@@ -10,7 +10,6 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:get_it_injectable_poc/foobar.dart' as _i4;
 import 'package:get_it_poc/get_it_poc.module.dart' as _i3;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -26,7 +25,6 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     await _i3.GetItPocPackageModule().init(gh);
-    gh.factory<_i4.FooBar>(() => const _i4.FooBar());
     return this;
   }
 }
